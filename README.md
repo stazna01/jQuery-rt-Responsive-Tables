8b002e26727334f24c364bac7d6961b4c10d9308
Nathan Stazewski <stazna01@gmail.com>
2014-02-11 10:16:37 -0800

    First Upload

---
+++ README.md
@@ -0,0 +1,73 @@
+# rt Responsive Tables jQuery Plugin
+
+rt Responsive Tables is a jQuery plugin that takes tables utilizing "th" tags and makes them vertical at widths where the table would no longer be able to fit its container.
+
+This plugin is built heavily upon the work by Chris Coyier found at http://css-tricks.com/responsive-data-tables/. The reason for this plugin, though, is that his method requires changing CSS breakpoints and the CSS that names the table columns in vertical mode. For static tables this is not a problem, but I needed something that would always work, no matter how the content in the table cells and headers might change. Instead of using hard-coded CSS for the column names, and breakpoints for when the table will no longer fit, this plugin is dynamic. It writes custom CSS into the head of the page, and calculates when to make the table vertical and when it can fit horizontally.
+
+This plugin has been tested and works with modern versions of Chrome, Firefox, Safari, iOS, and Android. It works perfectly with IE9+ but older versions of IE won't show the vertical column names while in vertical mode (due to CSS limitations in IE8 and lower).
+
+## Usage
+
+Include the plugin script and style file.
+
+'''html
+<link rel="stylesheet" href="jquery.rtResponsiveTables.min.css">
+<script src="jquery.rtResponsiveTables.min.js" type="text/javascript"></script>
+'''
+
+###Example Usage
+
+'''html
+<table>
+	<thead>
+	<tr>
+		<th>First Name</th>
+		<th>Middle Name</th>
+		<th>Last Name</th>
+		<th>Occupation</th>
+		<th>Date of Birth</th>
+		<th>Favorite Food</th>
+	</tr>
+	</thead>
+	<tbody>
+	<tr>
+		<td>Michael</td>
+		<td>Gary</td>
+		<td>Scott</td>
+		<td>Regional Manager, Dunder Mifflin, Scranton</td>
+		<td>March 15, 1964</td>
+		<td>"Everything" Pretzels</td>
+	</tr>
+	<tr>
+		<td>Dwight</td>
+		<td>Kurt</td>
+		<td>Schrute</td>
+		<td>Assistant to the Regional Manager, Dunder Mifflin, Scranton PA</td>
+		<td>January 20, 1968</td>
+		<td>Beets</td>
+	</tr>
+	</tbody>
+</table>
+
+<script type="text/javascript">
+$("table").rtResponsiveTables();
+</script>
+'''
+
+## Options
+
+**containerBreakPoint**: number in pixels (default is 0)
+
+There may be times when you want a table to become vertical even though it still technically can fit horizontally. The containerBreakPoint option allows you to give a number (in pixels) so that when the table's container hits that width, it will switch to vertical mode. If the table needs to switch before then, it will do so, but you can guarantee a table will at least switch by this number. 
+
+**Please note that this is NOT the viewport width, but rather the width of the table's containing element.**
+
+###Example Usage
+
+'''html
+<script type="text/javascript">
+$("table#nintendoTable").rtResponsiveTables({
+	containerBreakPoint: 1000
+	});
+</script>
+'''
---
+++ jquery.rtResponsiveTables.js
@@ -0,0 +1,98 @@
+/* jQuery rt Responsive Tables - v1.0.0 - 2014-02-10
+* https://github.com/stazna01/jQuery-rt-Responsive-Tables
+*
+* This plugin is built heavily upon the work by Chris Coyier
+* found at http://css-tricks.com/responsive-data-tables/
+*
+* Copyright (c) 2014 Nathan Stazewski; Licensed MIT */
+
+(function ( $ ) {
+$.fn.rtResponsiveTables = function( options ) {
+	// This is the easiest way to have default options.
+	var settings = $.extend({
+		// These are the defaults.
+		containerBreakPoint: 0 //allows a user to force the vertical mode at a certain pixel width of its container, in the case when a table may technically fit but you'd prefer the vertical mode
+		}, options );
+		
+	rtStartingOuterWidth = window.outerWidth; //used later to detect orientation change across all mobile browsers (other methods don't always work on Android)
+	is_iOS = /(iPad|iPhone|iPod)/g.test( navigator.userAgent ); //needed due to the fact that iOS scrolling causes false resizes
+	rt_responsive_table_object = this;
+	
+	function rt_write_css(rt_class_identifier) {
+		rt_css_code = '<style type="text/css">';
+		$(rt_class_identifier).find('th').each(function(index, element) {
+			rt_css_code += rt_class_identifier +'.rt-vertical-table td:nth-of-type('+(index+1)+'):before { content: "'+$(this).text()+'"; }';
+			});
+		rt_css_code += '</style>';
+		$(rt_css_code).appendTo('head');
+		}
+		
+	function determine_table_width (rt_table_object) { //outerWidth doesn't work properly in Safari if the table is overflowing its container
+		rt_table_width = 0;
+		if(rt_table_object.hasClass('rt-vertical-table')) {
+			rt_table_width = rt_table_object.outerWidth();
+			} else {
+				rt_table_object.find('th').each(function(index, element) {
+					rt_table_width += $(this).outerWidth();
+					});
+				rt_table_width = rt_table_width; //this seems to fix a rounding bug in firefox
+				}
+		return rt_table_width;
+		}
+	
+	function fix_responsive_tables() {
+		if ($("table.rt-responsive-table").length) {
+			
+			$("table.rt-responsive-table").each(function( index ) {
+				rt_containers_width = $(this).parent().width();
+				rt_current_width = determine_table_width($(this))-1;  //this "-1" seems to fix an issue in firefox without harming any other browsers
+				rt_max_width = $(this).attr('data-rt-max-width');
+				rt_has_class_rt_vertical_table = $(this).hasClass('rt-vertical-table');
+				if (rt_containers_width < rt_current_width || rt_containers_width <= settings.containerBreakPoint) { //the parent element is less than the current width of the table or the parent element is less than or equal to a user supplied breakpoint
+					$(this).addClass('rt-vertical-table'); //switch to vertical orientation (or at least keep it that orientation)
+					
+					if(rt_max_width > rt_current_width && rt_max_width > settings.containerBreakPoint) { //the max width was set too high and needs to be adjusted to this lower number
+							$(this).attr('data-rt-max-width', rt_current_width);
+							} else if (rt_max_width > rt_current_width && rt_max_width <= settings.containerBreakPoint) { //same as above but in this case the breakpoint is larger or equal so it needs to be set as the max width
+								$(this).attr('data-rt-max-width', settings.containerBreakPoint);
+								}
+							
+					} else if (rt_containers_width > rt_max_width && rt_containers_width > settings.containerBreakPoint) { //the parent element is bigger than the max width and user supplied breakpoint
+						$(this).removeClass('rt-vertical-table');  //switch to horizontal orientation (or at least keep it that orientation)
+						
+						if((rt_max_width > rt_current_width && !rt_has_class_rt_vertical_table) && (rt_max_width > settings.containerBreakPoint && !rt_has_class_rt_vertical_table)) { //max width is greater than the table's current width and it's in horizontal mode currently...so the max width was set to low and needs to be adjusted to a higher number
+							$(this).attr('data-rt-max-width', rt_current_width);
+							} else if((rt_max_width > rt_current_width && !rt_has_class_rt_vertical_table) && (rt_max_width <= settings.containerBreakPoint && !rt_has_class_rt_vertical_table)) { //same as above but in this case the user supplied breakpoint is larger or equal so it needs to be set as the max width
+								$(this).attr('data-rt-max-width', settings.containerBreakPoint);
+								}
+							
+						} else { //equal
+								
+								}
+				});			
+			}
+		}
+		
+	rt_responsive_table_object.each(function(index, element) {
+		$(this).addClass('rt-responsive-table-'+index).addClass('rt-responsive-table');
+		if (index == rt_responsive_table_object.length-1) {
+			$(window).resize(function() {
+				if(!is_iOS || (is_iOS && (rtStartingOuterWidth !== window.outerWidth))) {
+					rtStartingOuterWidth = window.outerWidth; //MUST update the starting width so future orientation changes will be noticed
+					fix_responsive_tables();
+					}
+				});
+			rt_responsive_table_count = $('table.rt-responsive-table').length;
+			$('table.rt-responsive-table').each(function(index2, element2) {
+				rt_write_css('table.rt-responsive-table-'+index2);
+				$('table.rt-responsive-table-'+index2).attr('data-rt-max-width', determine_table_width($(this)));
+				if (rt_responsive_table_count - 1 == index2) {
+					fix_responsive_tables();
+					}
+				});
+			}
+		});
+		
+	return this;
+	};
+}( jQuery ));
